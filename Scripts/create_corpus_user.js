3c7797a4b17cb5bd79d295db0ced859b3cfe4c0a
argicola <argicola.piger@yandex.ru>
2024-09-16 00:46:37 +0300

    db dump

--- Scripts/create_corpus_user.js
+++ Scripts/create_corpus_user.js
@@ -1,2 +1,2 @@
 use 'corpus';
-db.usermodel.insert( { "email": "georgiymolkov@gmail.com", "password": "$2b$10$.WhW4OZpIgq7OZQEIL9SiOkBuq/cCJ39Bj./eLXKm1QB9CxMvhrrC" } );
+db.usermodel.insert( { "email": "dariaspost1@gmail.com", "password": "$2b$10$uRPdEG4OET0XmpwVRz1h3OdSGTgp2Xt5jU2Jf/UET4XW10z3l2LJy", "name": "Суховольская Д. В." } );
--- Scripts/update_projectmodel.js
+++ Scripts/update_projectmodel.js
@@ -1,9 +1,6 @@
 use corpus;
 try {
-   db.projectmodel.updateMany(
-      { status: "deleted" },
-      { $set: {projectType: "dict"} }
-   );
+   db.projectmodel.update({ "_id" : ObjectId("601060b6db14f8437020442e") },{ $set: {"creatorId" : ObjectId("66b6060a8b5766f835597a1e")} });
 } catch (e) {
    print(e);
 }
